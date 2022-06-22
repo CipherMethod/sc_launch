9ca2576ecd6d423e5e620a7ac10f9041bd5a3dbc
Jamey Hopkins <35248862+CipherMethod@users.noreply.github.com>
2022-06-21 21:16:39 -0700

    A few improvements
    20220621 - Expand pgrep from sc_ to sc_serv
                         A more better display of what's running
                         Better process count maths

--- sc_launch.sh
+++ sc_launch.sh
@@ -4,12 +4,21 @@
 #20120606 - Launch SC Streaming Services
 #20141109 - Added check if running in order to cron launch after a reboot
 #20191122 - Use pgrep
+#20220621 - Expand pgrep from sc_ to sc_serv
+#           A more better display of what's running
 
-cd /home/sc
-COUNT=`pgrep sc_ | wc -l`
-if [ "$COUNT" -ge 2 ]; then
+HOME="/home/sc"
+
+cd $HOME
+echo "Process List:"
+ps -ef | grep sc_serv
+#pgrep sc_serv
+COUNT=`pgrep sc_serv | wc -l`
+echo "Process count: $COUNT"
+if [ "$COUNT" -gt 0 ]; then
    echo Shoutcast appears to be already running...
 else
+   echo "Starting SHOUTcast instance"
    sc2.6.0/sc_serv serv.conf &
    sleep 2
    ./sc_trans trans.conf &
